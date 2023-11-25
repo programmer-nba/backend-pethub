a5d51daeefa473b9f675bb2052073ad054894e37
warunyoo1 <warunyoo084@gmail.com>
2023-11-25 14:15:50 +0700

    dsdsd

--- models/product/preorder.model.js
+++ models/product/preorder.model.js
@@ -5,16 +5,16 @@ const PreOrderSchema = new mongoose.Schema({
   shop_id: {type: String, required: true},
   invoice : {type: String, required: false,},
   employee_name: {type: String, required: false},
-  product_detail: {
-    type: [
-      {
-        product_id: {type: String, required: false},
-        product_name: {type: String, required: false},
-        product_amount: {type: Number, required: false},
-        product_price: {type: Number, required: false},
-        product_logo: {type: Number, required: false},
-      },
-    ],
+  product_detail: {type: mongoose.Schema.Types.Mixed
+    // type: [
+    //   {
+    //     product_id: {type: String, required: false},
+    //     product_name: {type: String, required: false},
+    //     product_amount: {type: Number, required: false},
+    //     product_price: {type: Number, required: false},
+    //     product_logo: {type: Number, required: false},
+    //   },
+    // ],
   },
   status: {type: Array, required: false,},
   timestamps: {type: Date, required: false, default: Date.now()},
