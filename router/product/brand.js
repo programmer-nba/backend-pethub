0377e99f1001148b453b48ae7652dea2747e1f48
prakor <149643374+prakor@users.noreply.github.com>
2024-01-06 10:02:51 +0700

    06/01/2024

--- router/product/brand.js
+++ router/product/brand.js
@@ -3,10 +3,10 @@ const band = require("../../controllers/product/band.controller.js");
 const authAdmin = require("../../lib/auth.admin.js");
 
 
-router.post("/createBand",band.createBand)
-router.get("/getBandAll",band.getBandAll)
-router.get("/getBandOne/:id",band.getBandById)
-router.put("/updateBand/:id", band.updateband);
-router.delete("/deleteband/:id",band.deleteband)
+router.post("/createBand",authAdmin, band.createBand)
+router.get("/getBandAll",authAdmin, band.getBandAll)
+router.get("/getBandOne/:id",authAdmin, band.getBandById)
+router.put("/updateBand/:id",authAdmin, band.updateband);
+router.delete("/deleteband/:id",authAdmin, band.deleteband)
 
 module.exports = router; 
--- router/product/productGroup.js
+++ router/product/productGroup.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const productgroup = require("../../controllers/product/productGroup.controller.js");
 const authAdmin = require("../../lib/auth.admin.js");
 
-router.post("/createproductgroup",productgroup.createSize)
-router.get("/list/productgroup",productgroup.getSizeAll)
-router.get("/list/productgroup/:id",productgroup.getSizeById)
-router.put("/list/updateproductgroup/:id",productgroup.updateSize)
-router.delete("/list/deleteproductgroup/:id",productgroup.deleteSize)
+router.post("/createproductgroup",authAdmin, productgroup.createSize)
+router.get("/list/productgroup",authAdmin,productgroup.getSizeAll)
+router.get("/list/productgroup/:id",authAdmin, productgroup.getSizeById)
+router.put("/list/updateproductgroup/:id",authAdmin, productgroup.updateSize)
+router.delete("/list/deleteproductgroup/:id",authAdmin, productgroup.deleteSize)
 
 module.exports = router; 
